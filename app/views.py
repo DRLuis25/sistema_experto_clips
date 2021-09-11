2bcc899b8215bdfdbd6dcd335f14b2e18465c5dc
Luis Diego Plasencia Amaya <73008973+lplasencia@users.noreply.github.com>
2021-09-11 16:31:18 -0500

    Cambio en el select y reglas

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ import clips
 import os
 from pathlib import Path
 
+env = clips.Environment()
 
 def paginaIndex(request):
     context = {}
@@ -20,9 +21,25 @@ def paginaIndex(request):
     # env.build("reglas.clp")
     #for rule in env.rules():
     #    print(rule)
-    diagnostico1 = Diagnostico.objects.get(resultado='Diagnóstico prueba')
-    for asd in diagnostico1.sintomas.all():
-        print(asd.simbolo)
+
+    #Llenado de Diagnosticos
+    diagnosticos = Diagnostico.objects.all()
+    cont = 1
+    for d in diagnosticos.all():
+        print(d.simbolo)
+        diagnostico1 = Diagnostico.objects.get(simbolo=d.simbolo)
+        sintomas = ""
+        for asd in diagnostico1.sintomas.all():
+            sintomas += "(" + asd.simbolo + " S)"
+        rule = "(defrule diagnostico"+str(cont)+" "+sintomas+" => (assert (diagnostico "+d.simbolo+")))"
+        env.build(rule)
+        cont = cont + 1
+    env.reset()
+    #FIn llenado de Diagnosticos
+
+
+    context['sintomas'] = Sintoma.objects.all()
+    context['selected'] = ''
     return render(request, 'sintomas.html', context)
     return procesarView(request)
 
@@ -32,9 +49,7 @@ def procesarView(request):
     context['diagnostico'] = ''
     ##Some code
     if request.method == 'POST':
-        env = clips.Environment()
-        env.load("reglas.clp")
-        env.reset()
+
         sint = request.POST.getlist('sintomas[]')
         print('Sintomas seleccionados: ', sint)
         for sin in sint:
@@ -46,6 +61,7 @@ def procesarView(request):
             if fact.template.name == 'diagnostico':
                 print(fact[0])
                 context['diagnostico'] = 'El diagnóstico es: ' + fact[0]
+    context['selected'] = sint
     context['sintomas'] = Sintoma.objects.all()
     print('sintomas', context['sintomas'])
     print('diagnostico: ', context['diagnostico'])
