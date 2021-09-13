9a9270a23ae769f16d52a470e7fb02c84963cc6f
Luis Diego Plasencia Amaya <73008973+lplasencia@users.noreply.github.com>
2021-09-13 17:33:03 -0500

    Arreglos de estilos

--- app/admin.py
+++ app/admin.py
@@ -10,6 +10,11 @@ class DiagnosticoAdmin(admin.ModelAdmin):
     search_fields = ['resultado']
     list_per_page = 10
 
+class DiagnosticoUsuarioAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_per_page = 10
+    list_display = ('name','diagnostic')
+
 admin.site.register(Sintoma, SintomaAdmin)
 admin.site.register(Diagnostico, DiagnosticoAdmin)
-admin.site.register(DiagnosticoUsuario)
+admin.site.register(DiagnosticoUsuario, DiagnosticoUsuarioAdmin)
